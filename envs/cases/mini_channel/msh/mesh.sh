819a2d92729e1986203d564e74c11240e8e98af2
yuninw <yuninw@umich.edu>
2026-05-26 16:45:38 -0400

    final check on env

--- envs/cases/mini_channel/msh/mesh.sh
+++ envs/cases/mini_channel/msh/mesh.sh
@@ -1,5 +1,5 @@
-../../KTH_Framework/Nek5000/bin/genbox
-../../KTH_Framework/Nek5000/bin/genmap
+../../../solver/KTH_Framework/Nek5000/bin/genbox
+../../../solver/KTH_Framework/Nek5000/bin/genmap
 mv box.re2 ../phill.re2
 mv box.ma2 ../phill.ma2
 
